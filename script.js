108adf3999e74d434fccd8ff1e5635d439cbb0d7
Golibjon <uzcoder240@gmail.com>
2024-01-11 23:05:38 +0500

    Simple project

---
+++ script.js
@@ -0,0 +1,13 @@
+// var typed = new Typed((".skills"),{
+//     strings : ["Frontend developer","Youtuber","Ui designer"],
+//     typeSpeed : 100,
+//     backSpeed : 100,
+//     backDelay : 1000,
+//     loop : true
+// })
+
+
+let downloadBtn = document.getElementsByClassName('downloadBtn'),
+     contactBtn = document.getElementsByClassName('contactBtn');
+
+   
